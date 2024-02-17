c5ce95c32e4ac5a3c50ce559e3fd13b8227ba741
manu-xo5 <mohit.matwaya@gmail.com>
2024-02-18 01:58:12 +0530

    feat: make secondary monitor on left

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -22,6 +22,7 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("i", "<C-s>", "<cmd>:w<CR>", opts)
 
 -- Buffer keymap
 keymap("n", "<C-H>", "<C-W><C-H>", opts)
